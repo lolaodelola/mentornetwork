b851ca8736faf67c948bce5c773100d4e41f25e1
lolaodelola <damzcodes@gmail.com>
2020-10-28 15:01:10 +0000

    mentor can edit

--- app/controllers/mentors_controller.rb
+++ app/controllers/mentors_controller.rb
@@ -1,4 +1,4 @@
-class MentorsController < ApplicationController
+class MentorsController < Devise::RegistrationsController
 
   def index
     @mentors = Mentor.confirmed.all
@@ -18,14 +18,32 @@ class MentorsController < ApplicationController
 
   def edit
     @mentor = find_by_username
+    render 'mentors/registrations/edit'
   end
 
-  # Update
+  def update
+    mentor = find_by_username
+    remove_blank_passwords
+    # binding.pry
+    mentor.update!(mentor_params)
+  end
+
   # Delete
 
   private
   def find_by_username
     Mentor.find_by_username(params[:username])
+  end
+
+  def mentor_params
+    params.require(:mentor).permit(:username, :password, :password_confirmation, :email, tag_ids: [])
+  end
+
+  def remove_blank_passwords
+    if params.dig(:mentor, :password).blank? && params.dig(:mentor, :password_confirmation).blank?
+      params[:mentor].delete(:password)
+      params[:mentor].delete(:password_confirmation)
+    end
   end
 end
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,12 @@
 Rails.application.routes.draw do
-  devise_for :mentors
+  devise_for :mentors, controllers: { registrations: "mentors" }
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  resources :mentors, param: :username do
-    collection do
-      get '/results', to: 'mentors#results', as: 'results'
+
+  devise_scope :mentor do
+    resources :mentors, param: :username do
+      collection do
+        get '/results', to: 'mentors#results', as: 'results'
+      end
     end
   end
 end
