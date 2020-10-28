d678fe10981a2daa578594ca746d0b49af841d5e
lolaodelola <damzcodes@gmail.com>
2020-10-28 15:01:33 +0000

    devise init

---
+++ app/helpers/mentors_helper.rb
@@ -0,0 +1,14 @@
+module MentorsHelper
+
+  # def resource_name
+  #   :mentor
+  # end
+  #
+  # def resource
+  #   @resource ||= Mentor.new
+  # end
+  #
+  # def devise_mapping
+  #   @devise_mapping ||= Devise.mappings[:mentor]
+  # end
+end
