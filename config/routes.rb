1bd07fba5e96859b017bbdf404e88c847a99fddb
lolaodelola <damzcodes@gmail.com>
2020-10-29 13:34:18 +0000

    add footer

--- config/routes.rb
+++ config/routes.rb
@@ -10,5 +10,5 @@ Rails.application.routes.draw do
     end
   end
 
-  get '/privacy-policy', to: 'static#privacy_policy'
+  get '/privacy-policy', to: 'static#privacy_policy', as: 'privacy_policy'
 end
