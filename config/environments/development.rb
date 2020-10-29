cc3310055f8c48691d849c19cb75c1afd7246cc8
lolaodelola <damzcodes@gmail.com>
2020-10-29 14:09:20 +0000

    add domain host

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -60,5 +60,5 @@ Rails.application.configure do
   # routes, locales, etc. This feature depends on the listen gem.
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
 
-  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+  config.action_mailer.default_url_options = { host: ENV['DOMAIN'], port: 3000 }
 end
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -104,4 +104,5 @@ Rails.application.configure do
   # config.active_record.database_selector = { delay: 2.seconds }
   # config.active_record.database_resolver = ActiveRecord::Middleware::DatabaseSelector::Resolver
   # config.active_record.database_resolver_context = ActiveRecord::Middleware::DatabaseSelector::Resolver::Session
+  config.action_mailer.default_url_options = { host: ENV['DOMAIN'], port: 3000 }
 end
