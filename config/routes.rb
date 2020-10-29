8c4b45e3d27e89f5c36331db37d495c68c7a11ac
lolaodelola <damzcodes@gmail.com>
2020-10-29 09:24:11 +0000

    basic styling -wip

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
   devise_for :mentors, controllers: { registrations: "mentors" }
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
   devise_scope :mentor do
+    root 'mentors#root'
     resources :mentors, param: :username do
       collection do
         get '/results', to: 'mentors#results', as: 'results'
