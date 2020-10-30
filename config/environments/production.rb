fe124a49d3ecff72b9d2e7c76717ead560f17b5e
lolaodelola <damzcodes@gmail.com>
2020-10-30 14:39:23 +0000

    mailer set up

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -105,4 +105,14 @@ Rails.application.configure do
   # config.active_record.database_resolver = ActiveRecord::Middleware::DatabaseSelector::Resolver
   # config.active_record.database_resolver_context = ActiveRecord::Middleware::DatabaseSelector::Resolver::Session
   config.action_mailer.default_url_options = { host: ENV['DOMAIN'] }
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+      address:              'smtp.gmail.com',
+      port:                 587,
+      domain:               ENV['DOMAIN'],
+      user_name:            ENV['GMAIL_AD'],
+      password:             ENV['GMAIL_PW'],
+      authentication:       'plain',
+      enable_starttls_auto: true
+  }
 end
