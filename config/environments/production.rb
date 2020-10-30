369336bb3cd6c0d781becd00940c2d05754091a4
lolaodelola <damzcodes@gmail.com>
2020-10-30 15:10:52 +0000

    use ssl for mailer

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -108,11 +108,12 @@ Rails.application.configure do
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
       address:              'smtp.gmail.com',
-      port:                 587,
+      port:                 465,
       domain:               ENV['DOMAIN'],
       user_name:            ENV['GMAIL_AD'],
       password:             ENV['GMAIL_PW'],
       authentication:       'plain',
+      ssl:                   true,
       enable_starttls_auto: true
   }
 end
